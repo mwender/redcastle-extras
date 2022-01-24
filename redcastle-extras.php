811a880e0d3b768cac10d26dfa95d7f785f1adff
Michael Wender <michael@michaelwender.com>
2022-01-24 10:39:16 -0500

    First commit

---
+++ redcastle-extras.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Red Castle Extras
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Enhancers for the Red Castle website
+ * Author:          Michael Wender
+ * Author URI:      https://mwender.com
+ * Text Domain:     redcastle-extras
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Redcastle_Extras
+ */
+
+// Your code starts here.
